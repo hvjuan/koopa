3dc186b59532749ed10001a38e6835770c3d941a
Juan Hernandez <juan@xagnus.com>
2021-03-26 18:07:03 -0400

    Updating docstrings

--- l10n/translations.py
+++ l10n/translations.py
@@ -20,6 +20,8 @@ class Translate:
 
     Properties:
         base_path: project path to base translations document.
+        file_name: default file name. Can be added at instantiation time in case
+            is other than messages.po/messages.*.po.
     """
 
     def __init__(self, base_path: str, file_name: str = _MESSAGES):
@@ -30,12 +32,30 @@ class Translate:
         self.file_name = file_name
 
     def translate(self, key_path: str, locale='en-US'):
+        """Main translation method.
+
+        Args:
+            key_path: path to the translation key to use using dot notation.
+            locale: locale code to be using.
+        Returns:
+            Translated or default string.
+        """
         *key_path, translation_key = key_path.split('.')
         full_path = self._build_route(key_path, locale)
         return self._get_key(full_path, translation_key, locale)
 
     def _get_key(self, full_path: str, translation_key: str, locale: str):
-        # TODO(juan) what happens when we give it a wrong file?
+        """Gets translation key from localized files.
+
+        Args:
+            full_path: complete path to the corresponding .po file.
+            translation_key: translation key containing the localized text.
+        Returns:
+            Translated or default string.
+        Raises:
+            koopa_exceptions.KeyDoesNotExistException: raised if the given
+                key was not found.
+        """
         for entry in polib.pofile(full_path):
             if translation_key == entry.msgid:
                 return entry.msgstr
@@ -44,6 +64,23 @@ class Translate:
             f'The translation key {translation_key} was not found.')
 
     def _build_route(self, key_path: List[str], locale: str) -> str:
+        """Builds and returns route to the .po translation file.
+        
+        Builds it from the object path, self.file_name and locale.
+
+        Falls back to base locale if default hasn't changed or it wasn't
+        found on the file system. Fail silently but check again that at least
+        the base file exists, ie: messages.po
+
+        Args:
+            key_path: List of strings with the local path to the translation file.
+            locale: specific locale to get data from.
+        Returns: 
+            String with complete path to the .po file.
+        Raises:
+            koopa_exceptions.FileDoesNotExistException: raised if the final default file
+                path created does not exist.
+        """
         key_path = '/'.join(key_path)
 
         if locale is not _BASE_LOCALE:
@@ -52,9 +89,7 @@ class Translate:
             # Check if file exists, otherwise fall back to _BASE_LOCALE.
             if os.path.isfile(full_path):
                 return full_path
-        # Fall back to base locale if default hasn't changed or it wasn't
-        # found on the file system. Fail silently but check again that at least
-        # the base file exists, ie: messages.po
+        # Fallback.
         default = f'{self.base_path}/{key_path}/messages.po'
         if not os.path.isfile(default):
             raise koopa_exceptions.FileDoesNotExistException(
